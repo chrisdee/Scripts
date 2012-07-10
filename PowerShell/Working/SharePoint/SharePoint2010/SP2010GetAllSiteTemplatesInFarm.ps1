ff44d45a70d53ad001c9f80a0e9334760d268914
chrisdee <chrisdee@github.com>
2012-07-10 17:33:44 +0200

    SP2010 Scripts added

---
+++ PowerShell/Working/SharePoint/SharePoint2010/SP2010GetAllSiteTemplatesInFarm.ps1
@@ -0,0 +1,23 @@
+## SharePoint Server 2010: PowerShell Script To List Details On All Site Templates In A Farm ##
+## Resource: http://get-spscripts.com/2011/02/finding-site-template-names-and-ids-in.html
+
+Add-PSSnapin "Microsoft.Sharepoint.PowerShell" -ErrorAction SilentlyContinue
+
+function Get-SPWebTemplateWithId
+{
+    $templates = Get-SPWebTemplate | Sort-Object "Name"
+    $templates | ForEach-Object {
+        $templateValues = @{
+            "Title" = $_.Title
+            "Name" = $_.Name
+            "ID" = $_.ID
+            "Custom" = $_.Custom
+            "LocaleId" = $_.LocaleId
+        }
+        New-Object PSObject -Property $templateValues | Select @("Name","Title","LocaleId","Custom","ID")
+    }
+}
+
+# Examples:
+Get-SPWebTemplateWithId | Format-Table
+Get-SPWebTemplateWithId | Format-Table | Out-File "C:\BoxBuild\Scripts\PowerShell\SP2010_Site_Templates.txt"
---
+++ PowerShell/Working/SharePoint/SharePoint2010/SP2010ShowListFields.ps1
@@ -0,0 +1,9 @@
+﻿## SharePoint Server: PowerShell Script to Show All Fields In A List (Includes Custom and System Fields) ##
+## Resource: http://allaboutmoss.com/2010/05/11/3-ways-to-find-sharepoint-list-fields-internal-name
+## Environments: MOSS 2007 and SharePoint Server 2010
+
+[system.reflection.assembly]::loadwithpartialname("microsoft.sharepoint")
+$site= New-Object Microsoft.SharePoint.SPSite ("http://YourSPSite") #Change this to suit your environment
+$web=$site.OpenWeb()
+$list=$web.Lists["TestCustomList"] #Change this to your list name
+$list.Fields |select title, internalname| more

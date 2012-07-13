255a83b17a49a4832b1dfce5acae783498fb28c4
chrisdee <chrisdee@github.com>
2012-07-13 16:31:55 +0200

    Added PowerShell PSRemoting credentials and SQL connection scripts

---
+++ PowerShell/Working/PSRemoting/EncryptCredentialsForPSRemoting.ps1
@@ -0,0 +1,23 @@
+﻿## PowerShell Script to store and use the same credentials for PSRemoting scripting ##
+
+# Set your PowerShell Variables here
+
+$PasswordFileLocation = "C:\FolderName\psremotingpassword.txt" #Change the path here to suit your environment
+$PSRemotingUser = "DOMAIN\User" #Change the domain and user here to suit your environment
+$RemoteMachine = "MachineName" #Change the remote machine name here to suit your environment
+
+# Get host input to encrypt PSRemoting password to an encrypted text file
+
+Read-Host "Please enter your PSRemoting password" -AsSecureString | ConvertFrom-SecureString | out-file $PasswordFileLocation
+
+# Now read the PSRemoting password to a SecureString
+
+$pwd = Get-Content $PasswordFileLocation | ConvertTo-SecureString
+
+# Now create the credential to be used
+
+$crd = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $PSRemotingUser,$pwd
+
+# Now use your credentials for a new PSRemoting session
+
+Enter-PSSession -ComputerName $RemoteMachine -Authentication CredSSP -Credential $crd 
---
+++ PowerShell/Working/SQLServer/SQLServerConnectAndRunQueries.ps1
@@ -0,0 +1,46 @@
+﻿## PowerShell Script that queries SQL server and outputs the results to be imported for other commands ##
+## PowerShell Script creates a function to connect to and query a SQL Server ##
+
+function ConnectSQL {
+    Param ($server, $query, $database)
+    $conn = new-object ('System.Data.SqlClient.SqlConnection')
+    $connString = "Server=$server;Integrated Security=SSPI;Database=$database"
+    $conn.ConnectionString = $connString
+    $conn.Open()
+    $sqlCmd = New-Object System.Data.SqlClient.SqlCommand
+    $sqlCmd.CommandText = $query
+    $sqlCmd.Connection = $conn
+    $Rset = $sqlCmd.ExecuteReader()
+    ,$Rset ## The comma is used to create an outer array, which PS strips off automatically when returning the $Rset
+}
+
+function QuerySQL {
+    Param ($server, $query, $database = "master") ## Change your database here
+    $data = ConnectSQL $server $query $database
+    while ($data.read() -eq $true) {
+        $max = $data.FieldCount -1
+        $obj = New-Object Object
+        For ($i = 0; $i -le $max; $i++) {
+            $name = $data.GetName($i)
+            $obj | Add-Member Noteproperty $name -value $data.GetValue($i)
+     }
+     $obj
+    }
+}
+
+## Example: Connects to a SQL Instance and outputs results to a text file to be imported to make directories from these
+
+# Set your PowerShell Variables here
+
+$FolderLocation = "C:\temp"
+$SQLInstance = "D05479"
+$QueryResults = "QueryResults.txt"
+
+$Query1 = QuerySQL $SQLInstance "USE WebDeploy SELECT MachineName FROM ApplicationDetails Where Environment ='SAT'" | Out-File "$FolderLocation\$QueryResults"
+
+$FolderName = Get-Content "$FolderLocation\$QueryResults" | Select-String 'SAT'
+
+ForEach ($folder in $FolderName)
+{
+New-Item -Path $FolderLocation -Name $folder -ItemType directory
+}

ae54b032bd8aabb8c0ff8e1df0809fd9d88f1943
chrisdee <chrisdee@github.com>
2012-08-09 10:39:33 +0200

    Changes To SharePoint Content Type Queries

---
+++ PowerShell/Working/SharePoint/MOSS2007/SP2007GetContentTypeQueries.ps1
@@ -0,0 +1,61 @@
+﻿## SharePoint Server 2007: PowerShell Content Type Specific Queries ##
+## Overview: The 3 queries below cover Content Types used on SharePoint document and list libraries
+## Script 1: Get all lists where a specific content type has been applied to
+## Script 2: Get all list items where a specific content type has been applied to
+## Script 3: Gets a summary of all content types used on site collections and webs
+
+## 1. PowerShell Script To Show All Lists Where A Specific Content Type Has Been Used
+
+[System.Reflection.Assembly]::LoadWithPartialName("Microsoft.SharePoint")
+
+$site = New-Object Microsoft.SharePoint.SPSite("http://yourserver/sites/yoursite") #Change the URL to match your environment
+
+$webs = $site.AllWebs
+
+foreach ($web in $webs) 
+{
+  foreach ($lst in $web.lists) 
+  { 
+    foreach ($ctype in $lst.ContentTypes) 
+    { 
+      if ($ctype.Name -eq "Document") #Change your Site Content Type name here
+      { $lst.DefaultViewUrl }
+    }
+  } 
+  $web.Dispose() 
+}
+
+## 2. PowerShell Script To Show All List Items Where A Specific Content Type Has Been Used
+
+[System.Reflection.Assembly]::LoadWithPartialName("Microsoft.SharePoint")
+
+$site = New-Object Microsoft.SharePoint.SPSite("http://yourserver/sites/yoursite") #Change the URL to match your environment
+
+$webs = $site.AllWebs
+
+foreach ($web in $webs)
+{
+  foreach ($lst in $web.lists)
+  {
+    foreach ($item in $lst.Items)
+    {
+      if ($item.ContentType.Name -eq "Document") #Change your Site Content Type name here
+      { $item.Url}
+    }
+  }
+  $web.Dispose() 
+}
+
+## 3. PowerShell Script To Show All Content Types Used On Site Collections And Webs
+
+[System.Reflection.Assembly]::LoadWithPartialName("Microsoft.SharePoint")
+
+$site = New-Object Microsoft.SharePoint.SPSite("http://yourserver/sites/yoursite") #Change the URL to match your environment
+
+$webs = $site.AllWebs
+
+$web = $site.RootWeb
+
+foreach ($ctype in $web.ContentTypes) {$ctype.Name}
+
+$web.Dispose()
---
+++ PowerShell/Working/SharePoint/SharePoint2010/SP2010GetContentTypeQueries.ps1
@@ -0,0 +1,55 @@
+﻿## SharePoint Server 2010: PowerShell Content Type Specific Queries ##
+## Overview: The 3 queries below cover Content Types used on SharePoint document and list libraries
+## Script 1: Get all lists where a specific content type has been applied to
+## Script 2: Get all list items where a specific content type has been applied to
+## Script 3: Gets a summary of all content types used on site collections and webs
+
+## 1. PowerShell Script To Show All Lists Where A Specific Content Type Has Been Used
+
+Add-PSSnapin "Microsoft.SharePoint.PowerShell" -ErrorAction SilentlyContinue
+
+$webs = get-spsite "http://yourserver/sites/yoursite" | get-spweb #Change the URL to match your environment
+
+foreach ($web in $webs) 
+{
+  foreach ($lst in $web.lists) 
+  { 
+    foreach ($ctype in $lst.ContentTypes) 
+    { 
+      if ($ctype.Name -eq "Document") #Change your Site Content Type name here
+      { $lst.DefaultViewUrl }
+    }
+  } 
+  $web.Dispose() 
+}
+
+## 2. PowerShell Script To Show All List Items Where A Specific Content Type Has Been Used
+
+Add-PSSnapin "Microsoft.SharePoint.PowerShell" -ErrorAction SilentlyContinue
+
+$webs = get-spsite "http://yourserver/sites/yoursite" | get-spweb #Change the URL to match your environment
+
+foreach ($web in $webs)
+{
+  foreach ($lst in $web.lists)
+  {
+    foreach ($item in $lst.Items)
+    {
+      if ($item.ContentType.Name -eq "Document") #Change your Site Content Type name here
+      { $item.Url}
+    }
+  }
+  $web.Dispose() 
+}
+
+## 3. PowerShell Script To Show All Content Types Used On Site Collections And Webs
+
+Add-PSSnapin "Microsoft.SharePoint.PowerShell" -ErrorAction SilentlyContinue
+
+$site = Get-SPSite "http://yourserver/sites/yoursite" #Change the URL to match your environment
+
+$web = $site.RootWeb
+
+foreach ($ctype in $web.ContentTypes) {$ctype.Name}
+
+$web.Dispose()

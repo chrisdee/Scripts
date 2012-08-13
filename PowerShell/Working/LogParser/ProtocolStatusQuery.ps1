1cc07c3019f993e02cd2fb3f5d4971ffdcfe6a09
chrisdee <chrisdee@github.com>
2012-08-13 17:01:04 +0200

    Added Directory and Initial Script For PowerShell LogParser Queries

---
+++ PowerShell/Working/LogParser/ProtocolStatusQuery.ps1
@@ -0,0 +1,27 @@
+﻿## PowerShell: Using PowerShell To Run Log Parser Queries ## 
+## Overview: The Script below is an example of how you can use Log Parser with PowerShell to query IIS logs for Protocol Status messages
+## Requirements: Needs Log Parser 2.2 installed on the machine you run the script from
+## Usage Example: ./ProtocolStatusQuery.ps1 "\\ServerName\d$\Logs\IIS\*.log"
+
+$iisfiles = dir $args[0] #Variable for the IIS log files
+#The 2 variables make use of the LogParser COM Object
+$m = New-Object -comobject MSUtil.LogQuery
+$pif = New-Object -comobject MSUtil.LogQuery.IISW3CInputFormat
+
+foreach ($iisfile in $iisfiles) 
+{
+
+$SQL = "select DISTINCT cs-username, date, cs-uri-stem, sc-status from $iisfile where (sc-status >= 500 AND sc-status < 600) AND (cs-uri-stem LIKE '%.ASPX') "
+$recordSet = $m.Execute($SQL, $pif)
+
+"DATE        USER	   URL      				STATUS"
+"====================================================================="
+for(; !$recordSet.atEnd(); $recordSet.moveNext())
+{
+  $record=$recordSet.getRecord(); 
+  write-host ($record.GetValue(“date”).toshortdatestring() + “--” + $record.GetValue(“cs-username”) + “--” + $record.GetValue(“cs-uri-stem”) + “--”+ $record.GetValue(“sc-status”));
+}
+
+$recordSet.Close(); 
+
+}

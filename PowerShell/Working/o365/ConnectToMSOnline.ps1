6c7763541ded0735877a4e2474e0b47ed260f7ca
chrisdee <chrisdee@github.com>
2012-08-14 11:11:02 +0200

    Added o365 PowerShell Commands Directory And Connection Script

---
+++ PowerShell/Working/o365/ConnectToMSOnline.ps1
@@ -0,0 +1,21 @@
+﻿## o365: PowerShell Commands For The Online Services Module ##
+## You need to have the Online Services Module installed: http://g.microsoftonline.com/0BX10EN/423
+
+Import-Module MSOnline
+Import-Module MSOnlineExtended
+# launches the prompt for your 'onmicrosoft.com' account credentials
+$cred=Get-Credential
+Connect-MsolService -Credential $cred
+
+##Commandlets Resource: http://onlinehelp.microsoft.com/office365-enterprises/hh125002
+##Other Resources: 
+#http://blogs.technet.com/b/msukucc/archive/2011/08/18/office-365-non-federated-identity-password-never-expires.aspx
+#http://community.office365.com/en-us/wikis/sso/2062.aspx
+
+# Get-MsolSubscription
+# Get-MsolAccountSku
+# Get-MsolDomainFederationSettings
+# Get-MsolFederationProperty
+# Get-MSOLUser -DomainName YourDomainName.com
+# Get-MsolUser –UserPrincipalName UserName@YourDomain.onmicrosoft.com | fl 
+# Set-MsolUser –UserPrincipalName UserName@YourDomain.onmicrosoft.com -PasswordNeverExpires $True

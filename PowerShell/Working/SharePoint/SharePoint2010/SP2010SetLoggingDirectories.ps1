f4d338bebc232647fbe79e4c26861c09dd297354
chrisdee <chrisdee@github.com>
2012-09-13 17:44:14 +0200

    Changed PowerShell PSRemoting File Name

--- PowerShell/Working/SharePoint/SharePoint2010/SP2010SetLoggingDirectories.ps1
+++ PowerShell/Working/SharePoint/SharePoint2010/SP2010SetLoggingDirectories.ps1
@@ -1,10 +1,19 @@
 ## PowerShell Script to create and reconfigure your locations for your Diagnostic and Usage Analysis Logs ##
 
+## Overview: PowerShell script to create and configure key parameters related to Trace Logs and Usage Analysis Processing within SharePoint Server 2010
+
+## Note: The excellent AutoSPInstaller lets your configure most of these within the 'input' XML file - http://autospinstaller.codeplex.com
+
 # Edit the parameters below to suit your environment
 
-$DiagnosticLogs = "D:\SharePoint\DiagnosticLogs"
+$DiagnosticLogsPath = "D:\SharePoint\DiagnosticLogs"
 $DiagnosticLogsDays = "3"
-$UsageAnalysis = "D:\SharePoint\UsageAnalysis"
+$DiagnosticLogMaxDiskSpaceUsageEnabled = $True #Default should be '$True'
+$DiagnosticLogDiskSpaceUsageGB = "20" #Default is 1000 GB
+$DiagnosticLogCutInterval = "30" #Default is 30 minutes
+$UsageAnalysisPath = "D:\SharePoint\UsageAnalysis"
+$UsageAnalysisLogMaxSpaceGB = "5" #Default is 5 GB
+$UsageAnalysisLogCutTime = "5" #Default is 5 minutes
 
 Add-PSSnapin Microsoft.SharePoint.PowerShell
 
@@ -13,19 +22,19 @@ Add-PSSnapin Microsoft.SharePoint.PowerShell
 net stop SPTraceV4
 net stop SPTimerV4
 
-# Get-SPDiagnosticConfig
+# Tip: Use 'Get-SPDiagnosticConfig' to view additional parameters that can be set
 
-md $DiagnosticLogs
+md $DiagnosticLogsPath
 
-Set-SPDiagnosticConfig -LogLocation $DiagnosticLogs -DaysToKeepLogs $DiagnosticLogsDays
-
-
-# Get-SPUsageService
+Set-SPDiagnosticConfig -LogLocation $DiagnosticLogsPath -DaysToKeepLogs $DiagnosticLogsDays -LogMaxDiskSpaceUsageEnabled:$DiagnosticLogMaxDiskSpaceUsageEnabled -LogDiskSpaceUsageGB $DiagnosticLogDiskSpaceUsageGB -LogCutInterval $DiagnosticLogCutInterval
 
 
-md $UsageAnalysis
+# Tip: Use 'Get-SPUsageService' to view additional parameters that can be set
 
-Set-SPUsageService -UsageLogLocation $UsageAnalysis
+
+md $UsageAnalysisPath
+
+Set-SPUsageService -UsageLogLocation $UsageAnalysisPath -UsageLogMaxSpaceGB $UsageAnalysisLogMaxSpaceGB -UsageLogCutTime $UsageAnalysisLogCutTime
 
 
 #Start your SharePoint Services
@@ -33,8 +42,8 @@ Set-SPUsageService -UsageLogLocation $UsageAnalysis
 net start SPTraceV4
 net start SPTimerV4
 
-Write-Host Diagnostic Logs now at $DiagnosticLogs
-Write-Host Usage Analysis Logs now at $UsageAnalysis
+Write-Host Diagnostic Logs now at $DiagnosticLogsPath
+Write-Host Usage Analysis Logs now at $UsageAnalysisPath
 
 ## Important - If using the 'AutoSPInstaller' solution:
 # The EnterpriseSearchService Index location should have been set in your 'AutoSPInstallerInput' XML file

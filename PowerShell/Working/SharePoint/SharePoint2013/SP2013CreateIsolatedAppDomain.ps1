e686fc0efe4de343b9fc8179b9d5c41674b6fb77
chrisdee <chrisdee@github.com>
2012-10-22 10:03:41 +0200

    Added Initial SharePoint Server 2013 PowerShell Scripts

---
+++ PowerShell/Working/SharePoint/SharePoint2013/SP2013CreateIsolatedAppDomain.ps1
@@ -0,0 +1,84 @@
+﻿## SharePoint Server 2013: PowerShell Script To Provision An Isolated App Domain For Custom Applications ##
+
+<#
+
+Overview: The script below provisions and configures an Isolated App Domain for your on-premise farm
+
+Important: You will also need to configure your DNS settings for the new App Domain as per the last resource link below
+
+Resources: http://tomvangaever.be/blogv2/2012/08/prepare-sharepoint-2013-server-for-app-development-create-an-isolated-app-domain
+           http://msdn.microsoft.com/en-us/library/fp179923(v=office.15).aspx
+           http://sharepointchick.com/archive/2012/07/29/setting-up-your-app-domain-for-sharepoint-2013.aspx
+ #>
+
+# Check if the execution policy is set to Unrestricted
+$policy = Get-ExecutionPolicy
+if($policy -ne "Unrestricted"){
+	Set-ExecutionPolicy "Unrestricted"
+}
+
+# Check if current script is running under administrator credentials
+$currentPrincipal = New-Object Security.Principal.WindowsPrincipal( [Security.Principal.WindowsIdentity]::GetCurrent() )
+if ($currentPrincipal.IsInRole( [Security.Principal.WindowsBuiltInRole]::Administrator ) -eq $false) { 
+	(get-host).UI.RawUI.Backgroundcolor="DarkRed" 
+	clear-host 
+	write-host "Warning: PowerShell is not running as an Administrator.`n" 
+	exit
+}
+
+# Load SharePoint powershell commands
+Add-PSSnapin "microsoft.sharepoint.powershell" -ErrorAction SilentlyContinue
+
+cls
+
+# Ensure that the spadmin and sptimer services are running
+Write-Host
+Write-Host "Ensure that the spadmin and sptimer services are running" -ForegroundColor Yellow
+net start spadminv4
+net start sptimerv4
+
+# Create your isolated app domain by running the SharePoint Management Shell as an administrator and typing the following command.
+Write-Host
+Write-Host "Create your isolated app domain by running the SharePoint Management Shell as an administrator and typing the following command." -ForegroundColor Yellow
+$appdomain = Read-Host "Your App Domain Name"
+Set-SPAppDomain $appdomain
+
+# Ensure that the SPSubscriptionSettingsService and AppManagementServiceInstance services are running 
+Write-Host
+Write-Host "Ensure that the SPSubscriptionSettingsService and AppManagementServiceInstance services are running." -ForegroundColor Yellow
+Get-SPServiceInstance | where{$_.GetType().Name -eq "AppManagementServiceInstance" -or $_.GetType().Name -eq "SPSubscriptionSettingsServiceInstance"} | Start-SPServiceInstance
+
+# Verify that the SPSubscriptionSettingsService and AppManagementServiceInstance services are running 
+Write-Host
+Write-Host "Verify that the SPSubscriptionSettingsService and AppManagementServiceInstance services are running." -ForegroundColor Yellow
+Get-SPServiceInstance | where{$_.GetType().Name -eq "AppManagementServiceInstance" -or $_.GetType().Name -eq "SPSubscriptionSettingsServiceInstance"}
+
+# Specify an account, application pool, and database settings for the SPSubscriptionService and AppManagementServiceInstance services 
+Write-Host
+Write-Host "Specify an account, application pool, and database settings for the SPSubscriptionService and AppManagementServiceInstance services." -ForegroundColor Yellow
+$login = Read-Host "The login of a managed account"
+$account = Get-SPManagedAccount $login 
+$appPoolSubSvc = New-SPServiceApplicationPool -Name SettingsServiceAppPool -Account $account
+Write-Host "SettingsServiceAppPool created (1/6)" -ForegroundColor Green
+$appPoolAppSvc = New-SPServiceApplicationPool -Name AppServiceAppPool -Account $account
+Write-Host "AppServiceAppPool created  (2/6)" -ForegroundColor Green
+$appSubSvc = New-SPSubscriptionSettingsServiceApplication –ApplicationPool $appPoolSubSvc –Name SettingsServiceApp –DatabaseName SettingsServiceDB 
+Write-Host "SubscriptionSettingsServiceApplication created  (3/6)" -ForegroundColor Green
+$proxySubSvc = New-SPSubscriptionSettingsServiceApplicationProxy –ServiceApplication $appSubSvc
+Write-Host "SubscriptionSettingsServiceApplicationProxy created  (4/6)" -ForegroundColor Green
+$appAppSvc = New-SPAppManagementServiceApplication -ApplicationPool $appPoolAppSvc -Name AppServiceApp -DatabaseName AppServiceDB
+Write-Host "AppManagementServiceApplication created  (5/6)" -ForegroundColor Green
+$proxyAppSvc = New-SPAppManagementServiceApplicationProxy -ServiceApplication $appAppSvc
+Write-Host "AppManagementServiceApplicationProxy created  (6/6)" -ForegroundColor Green
+
+# Specify your tenant name 
+write-host
+Write-Host "Set AppSiteSubscriptionName to 'app'" -ForegroundColor Yellow
+Set-SPAppSiteSubscriptionName -Name "app" -Confirm:$false
+Write-Host "AppSiteSubscriptionName set" -ForegroundColor Green
+
+# Disable the loopbackcheck in the registry
+Write-Host "Disable the loopbackcheck in the registry" -ForegroundColor Yellow
+New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa\" -Name "DisableLoopbackCheck" -PropertyType DWord -Value 1
+
+Write-Host "Completed"
---
+++ PowerShell/Working/SharePoint/SharePoint2013/SP2013DownloadPreReqs.ps1
@@ -0,0 +1,64 @@
+﻿## SharePoint Server 2013: PowerShell Script To Download SP2013 Prerequisites ##
+## Resources: http://www.dontpapanic.com/blog/?p=241; http://dontpapanic.com/download/DownloadAllSP2013PreReqs.txt
+
+Import-Module BitsTransfer
+## Prompt for the destination path
+$DestPath = Read-Host -Prompt "- Enter the destination path for downloaded files"
+## Check that the path entered is valid
+If (Test-Path "$DestPath" -Verbose)
+{
+	## If destination path is valid, create folder if it doesn't already exist
+	$DestFolder = "$DestPath\PrerequisiteInstallerFiles"
+	New-Item -ItemType Directory $DestFolder -ErrorAction SilentlyContinue
+}
+Else
+{
+	Write-Warning " - Destination path appears to be invalid."
+	## Pause
+	Write-Host " - Please check the path, and try running the script again."
+	Write-Host "- Press any key to exit..."
+	$null = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+	break
+}
+## We use the hard-coded URL below, so that we can extract the filename (and use it to get destination filename $DestFileName)
+## Note: These URLs are subject to change at Microsoft's discretion - check the permalink next to each if you have trouble downloading.
+$UrlList = ("http://download.microsoft.com/download/D/0/F/D0F564A3-6734-470B-9772-AC38B3B6D8C2/dotNetFx45_Full_x86_x64.exe", # Microsoft .NET Framework 4.5
+            "http://download.microsoft.com/download/5/2/B/52B59966-3009-4F39-A99E-3732717BBE2A/Windows6.1-KB2506143-x64.msu", # Windows Management Framework 3.0 (CTP2)
+	    "http://download.microsoft.com/download/9/1/3/9138773A-505D-43E2-AC08-9A77E1E0490B/1033/x64/sqlncli.msi", #Microsoft SQL Server 2008 r2 Native Client
+	    "http://download.microsoft.com/download/D/7/2/D72FD747-69B6-40B7-875B-C2B40A6B2BDD/Windows6.1-KB974405-x64.msu", #Windows Identity Foundation (KB974405)
+	    "http://download.microsoft.com/download/E/0/0/E0060D8F-2354-4871-9596-DC78538799CC/Synchronization.msi", # Microsoft Sync Framework Runtime v1.0 SP1 (x64) 
+	    "http://download.microsoft.com/download/A/6/7/A678AB47-496B-4907-B3D4-0A2D280A13C0/WindowsServerAppFabricSetup_x64.exe", #Windows Server AppFabric
+            "http://download.microsoft.com/download/0/1/D/01D06854-CA0C-46F1-ADBA-EBF86010DCC6/r2/MicrosoftIdentityExtensions-64.msi", # Windows Identity Extensions
+            "http://download.microsoft.com/download/9/1/D/91DA8796-BE1D-46AF-8489-663AB7811517/setup_msipc_x64.msi", # Microsoft Information Protection and Control Client
+            "http://download.microsoft.com/download/8/F/9/8F93DBBD-896B-4760-AC81-646F61363A6D/WcfDataServices.exe", # Microsoft WCF Data Services 5.0
+            "http://download.microsoft.com/download/7/B/5/7B51D8D1-20FD-4BF0-87C7-4714F5A1C313/AppFabric1.1-RTM-KB2671763-x64-ENU.exe" # CU Package 1 for Microsoft AppFabric 1.1 for Windows Server (KB2671763)
+			)
+ForEach ($Url in $UrlList)
+{
+	## Get the file name based on the portion of the URL after the last slash
+	$DestFileName = $Url.Split('/')[-1]
+	Try
+	{
+		## Check if destination file already exists
+		If (!(Test-Path "$DestFolder\$DestFileName"))
+		{
+			## Begin download
+			Start-BitsTransfer -Source $Url -Destination $DestFolder\$DestFileName -DisplayName "Downloading `'$DestFileName`' to $DestFolder" -Priority High -Description "From $Url..." -ErrorVariable err
+			If ($err) {Throw ""}
+		}
+		Else
+		{
+			Write-Host " - File $DestFileName already exists, skipping..."
+		}
+	}
+	Catch
+	{
+		Write-Warning " - An error occurred downloading `'$DestFileName`'"
+		break
+	}
+}
+## View the downloaded files in Windows Explorer
+Invoke-Item $DestFolder
+## Pause
+Write-Host "- Downloads completed, press any key to exit..."
+$null = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")

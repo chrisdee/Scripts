8eb9c8e5afb93b8dc576ab7062326101cdbc6bfe
chrisdee <chrisdee@github.com>
2012-11-06 17:33:53 +0100

    Added 'GetUserPasswordLastSetDetails' AD Script

---
+++ PowerShell/Working/AD/GetUserPasswordLastSetDetails.ps1
@@ -0,0 +1,9 @@
+﻿## PowerShell: Script to Query Active Directory to Get Password Last Set and Password Expiration Details ##
+
+## Usage Example: Get-UserDetails 'cdee'
+
+Import-Module ActiveDirectory
+
+function Get-UserDetails([string]$user) {
+Get-ADUser $user -Properties PasswordLastSet, PasswordNeverExpires #Tip: Use a '*' after properties to get all available attributes related to a user
+}

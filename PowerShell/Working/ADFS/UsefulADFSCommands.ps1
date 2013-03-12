d5a1d104c206c293eb27cd47b01889ed05bf43df
chrisdee <chrisdee@github.com>
2013-03-12 16:25:18 +0100

    Added Useful ADFS Server PowerShell Commands

---
+++ PowerShell/Working/ADFS/UsefulADFSCommands.ps1
@@ -0,0 +1,20 @@
+﻿## ADFS Server: Useful Powershell Commandlets To Run On Your ADFS Server ##
+
+Add-PSSnapin “microsoft.adfs.powershell” -ErrorAction SilentlyContinue
+
+# Get ADFS Configuration Properties
+
+Get-ADFSProperties
+
+# Enable or Disable Auto Certificate Rollover
+
+Set-ADFSProperties -AutoCertificateRollover $true
+Set-ADFSProperties -AutoCertificateRollover $false
+
+# Set ADFS Self-Signed Certificates Duration
+
+Set-AdfsProperties -CertificateDuration 730 #Specify number of days
+
+# Update ADFS Self-Signed Certificates
+
+Update-AdfsCertificate -Urgent

4dfaab31f1fd0671862602e4d6e1c5e7d557bf6c
chrisdee <chrisdee@github.com>
2013-04-09 14:17:36 +0200

    Added SharePoint PowerShell Navigation Links Reporting Scripts

---
+++ PowerShell/Working/SharePoint/SharePoint2010/SP2010EnumNavigationLinks.ps1
@@ -0,0 +1,268 @@
+﻿## SharePoint Server: PowerShell Object Model Script Query To Produce A Report On SharePoint Navigation Links ##
+
+<# 
+
+PowerShell Object Model Script that goes through a MOSS site and produces a CSV file report of the following links:
+
+- Top/Global Navigation
+- Quick Launch
+- Links Lists
+
+Overview: This script will print out a list of all links in the Quick Launch, Links Lists, and default
+page for each SPWeb object to a CSV file located in the directory specified in the variables.
+
+Environments: MOSS 2007 and SharePoint Server 2010/2013 Farms
+
+Resource: http://blog.henryong.com/2011/05/20/sharepoint-link-reporter-using-powershell
+
+Notes: These columns are the most important for top navigation links: WebTitle; QLHeaderTitle;
+QLChildLinkTitle; and QLChildLink.
+
+#>
+
+######################## Start Variables ##############################################
+$siteURL = "http://YOURURLHERE" #URL to any sites in the web application.
+$filePath = "C:\PowerShellScripts\Links.csv" #Change this path to suit your environment
+$PublishingFeatureGUID = "94c94ca6-b32f-4da9-a9e3-1f3d343d7ecb"
+######################## End Variables ################################################
+
+
+if(Test-Path $filePath)
+{
+    Remove-Item $filePath
+}
+Clear-Host
+
+[System.Reflection.Assembly]::LoadWithPartialName("Microsoft.SharePoint")
+[System.Reflection.Assembly]::LoadWithPartialName("Microsoft.SharePoint.Publishing")
+[System.Reflection.Assembly]::LoadWithPartialName("System.Net.WebClient")
+
+# Creates an object that represents an SPWeb's Title and URL
+function CreateNewWebObject
+{
+    $linkObject = New-Object system.Object
+    $linkObject | Add-Member -type NoteProperty -Name WebTitle -Value $web.Title
+    $linkObject | Add-Member -type NoteProperty -Name WebURL -Value $web.URL
+    
+    return $linkObject
+}
+
+# Creates an object that represents the header links of the Quick Launch
+function CreateNewLinkHeaderObject
+{
+    $linkObject = New-Object system.Object
+    $linkObject | Add-Member -type NoteProperty -Name WebTitle -Value $prevWebTitle
+    $linkObject | Add-Member -type NoteProperty -Name WebURL -Value $prevWebURL            
+    $linkObject | Add-Member -type NoteProperty -Name QLHeaderTitle -Value $node.Title
+    $linkObject | Add-Member -type NoteProperty -Name QLHeaderLink -Value $node.Url
+
+    return $linkObject    
+}
+
+# Creates an object that represents to the links in the Top Link bar
+function CreateNewTopLinkObject
+{
+    $linkObject = New-Object system.Object
+    $linkObject | Add-Member -type NoteProperty -Name WebTitle -Value $prevWebTitle
+    $linkObject | Add-Member -type NoteProperty -Name WebURL -Value $prevWebURL            
+    $linkObject | Add-Member -type NoteProperty -Name TopLinkTitle -Value $node.Title
+    $linkObject | Add-Member -type NoteProperty -Name TopLinkURL -Value $node.Url
+    $linkObject | Add-Member -type NoteProperty -Name TopNavLink -Value $true
+
+    return $linkObject    
+}
+
+# Creates an object that represents the links of in the Quick Launch (underneath the headers)
+function CreateNewLinkChildObject
+{
+    $linkObject = New-Object system.Object
+    $linkObject | Add-Member -type NoteProperty -Name WebTitle -Value $prevWebTitle
+    $linkObject | Add-Member -type NoteProperty -Name WebURL -Value $prevWebURL
+    $linkObject | Add-Member -type NoteProperty -Name QLHeaderTitle -Value $prevHeaderTitle
+    $linkObject | Add-Member -type NoteProperty -Name QLHeaderLink -Value $prevHeaderLink
+    $linkObject | Add-Member -type NoteProperty -Name QLChildLinkTitle -Value $childNode.Title
+    $linkObject | Add-Member -type NoteProperty -Name QLChildLink -Value $childNode.URL
+
+    return $linkObject
+}
+
+
+## Creates an object that represents items in a Links list. 
+function CreateNewLinkItemObject
+{
+    $linkObject = New-Object system.Object
+    $linkObject | Add-Member -type NoteProperty -Name WebTitle -Value $prevWebTitle
+    $linkObject | Add-Member -type NoteProperty -Name WebURL -Value $prevWebURL
+    $linkObject | Add-Member -type NoteProperty -Name ListName -Value $list.Title
+    
+    $spFieldURLValue = New-Object microsoft.sharepoint.spfieldurlvalue($item["URL"])
+    
+    $linkObject | Add-Member -type NoteProperty -Name ItemTitle -Value $spFieldURLValue.Description
+    $linkObject | Add-Member -type NoteProperty -Name ItemURL -Value $spFieldURLValue.Url
+
+    return $linkObject
+}
+
+# Determines whether or not the passed in Feature is activated on the site or not. 
+function FeatureIsActivated
+{param($FeatureID, $Web)
+
+    return $web.Features[$FeatureID] -ne $null
+
+}
+
+# Creates an object that represents a link within the body of a content page. 
+function CreateNewPageContentLinkObject
+{
+    $linkObject = New-Object system.Object
+    $linkObject | Add-Member -type NoteProperty -Name WebTitle -Value $prevWebTitle
+    $linkObject | Add-Member -type NoteProperty -Name WebURL -Value $prevWebURL
+    $linkObject | Add-Member -type NoteProperty -Name PageContentLink -Value $link
+    
+    return $linkObject
+}
+
+$wc = New-Object System.Net.WebClient
+$wc.UseDefaultCredentials = $true
+$pattern = "(((f|ht){1}tp://)[-a-zA-Z0-9@:%_\+.~#?&//=]+)"
+
+$site = new-object microsoft.sharepoint.spsite($siteURL)
+$webApp = $site.webapplication
+$allSites = $webApp.sites
+
+$customLinkObjects =@()
+
+foreach ($site in $allSites)
+{
+    $allWebs = $site.AllWebs
+    
+    foreach ($web in $allWebs)
+    {
+        ## If the web has the publishing feature turned OFF, use this method
+        if((FeatureIsActivated $PublishingFeatureGUID $web) -ne $true)
+        {
+            $quickLaunch = $web.Navigation.QuickLaunch
+            $customLinkObject = CreateNewWebObject 
+
+            $customLinkObjects += $customLinkObject
+            
+            $prevWebTitle = $customLinkObject.WebTitle
+            $prevWebURL = $customLinkObject.WebURL
+            
+            # First level of the Quick Launch (Headers)
+            foreach ($node in $quickLaunch)
+            {
+                $customLinkObject = CreateNewLinkHeaderObject
+                
+                $customLinkObjects += $customLinkObject
+                
+                $prevHeaderTitle = $node.Title
+                $prevHeaderLink = $node.Url
+                
+                # Second level of the Quick Launch (Links)
+                foreach ($childNode in $node.Children)
+                {
+                    $customLinkObject = CreateNewLinkChildObject
+                    
+                    $customLinkObjects += $customLinkObject
+                }
+            }
+            
+            # Get all the links in the Top Link bar
+            $topLinks = $web.Navigation.TopNavigationBar
+            foreach ($node in $topLinks)
+            {
+                $customLinkObject = CreateNewTopLinkObject
+                
+                $customLinkObjects += $customLinkObject
+                
+                $prevHeaderTitle = $node.Title
+                $prevHeaderLink = $node.Url                
+            }
+        }
+        
+        ## If the web has the publishing feature turned ON, use this method
+        else
+        {
+            $publishingWeb = [Microsoft.SharePoint.Publishing.PublishingWeb]::GetPublishingWeb($web)
+            $quickLaunch = $publishingWeb.CurrentNavigationNodes
+            $customLinkObject = CreateNewWebObject 
+            $customLinkObjects += $customLinkObject
+            
+            $prevWebTitle = $customLinkObject.WebTitle
+            $prevWebURL = $customLinkObject.WebURL
+            
+            # First level of the Quick Launch (Headers)
+            foreach ($node in $quickLaunch)
+            {
+                $customLinkObject = CreateNewLinkHeaderObject
+                
+                $customLinkObjects += $customLinkObject
+                
+                $prevHeaderTitle = $node.Title
+                $prevHeaderLink = $node.Url
+                
+                # Second level of the Quick Launch (Links)
+                foreach ($childNode in $node.Children)
+                {
+                    $customLinkObject = CreateNewLinkChildObject
+                    
+                    $customLinkObjects += $customLinkObject
+                }
+            }
+            
+            # Get all the links in the Top Link bar
+            $topLinks = $web.Navigation.TopNavigationBar
+            foreach ($node in $topLinks)
+            {
+                $customLinkObject = CreateNewTopLinkObject
+                
+                $customLinkObjects += $customLinkObject
+                
+                $prevHeaderTitle = $node.Title
+                $prevHeaderLink = $node.Url                
+            }            
+            
+        }
+        
+        #Looking for lists of type Links
+        $lists = $web.Lists
+        foreach ($list in $lists)
+        {
+            if($list.BaseTemplate -eq "Links")
+            {
+                $prevWebTitle = $customLinkObject.WebTitle
+                $prevWebURL = $customLinkObject.WebURL
+                
+                # Going through all the links in a Links List
+                foreach ($item in $list.Items)
+                {
+                    $customLinkObject = CreateNewLinkItemObject
+                    
+                    $customLinkObjects += $customLinkObject                    
+                }
+            }    
+            Write-Host $list.title    
+        }
+        
+        #Looking at the default page for each web for links embedded within the content areas
+
+        $htmlContent = $wc.DownloadString($web.URL)
+
+        $result = $htmlContent | Select-String -Pattern $pattern -AllMatches
+        $links = $result.Matches | ForEach-Object {$_.Groups[1].Value}
+        foreach ($link in $links)
+        {
+            $customLinkObject = CreateNewPageContentLinkObject
+            $customLinkObjects += $customLinkObject
+        }
+
+        Write-Host $web.title
+        $web.Dispose()
+    }
+}
+
+# Exporting the data to a CSV file
+$customLinkObjects | Select-Object WebTitle,WebURL,TopNavLink,TopLinkTitle,TopLinkURL,QLHeaderTitle,QLHeaderLink,QLChildLinkTitle,QLChildLink,ListName,ItemTitle,ItemURL,PageContentLink | Export-Csv $filePath
+$site.dispose()
+write-host "Script completed, check your specified file path for the report: $filePath"

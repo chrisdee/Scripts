f33781b8ab6e249dee91054dcc51c363bccec5c4
chrisdee <chrisdee@github.com>
2013-07-24 15:39:34 +0200

    Added 'SP2013 Export Solutions' PowerShell Script

---
+++ PowerShell/Working/SharePoint/SharePoint2013/SP2013ExportAllSolutionsFromSolutionStore.ps1
@@ -0,0 +1,17 @@
+﻿## SharePoint Server 2013: PowerShell Script To Export All WSP Solutions From The Farm Solution Store ##
+## Resource: http://allaboutmoss.com/2011/10/16/export-wsp-from-farm-solution-store-using-powershell
+
+Add-PSSnapin "Microsoft.SharePoint.PowerShell" -ErrorAction SilentlyContinue
+
+foreach($solution in Get-SPSolution)
+{
+    try
+        {
+        $filename = $solution.Name;
+        $solution.SolutionFile.SaveAs("C:\BoxBuild\Solutions\$filename") #Change this path to suit your environment
+        }
+    catch
+        {
+        Write-Host "-error:$_"-foreground red
+        }
+}

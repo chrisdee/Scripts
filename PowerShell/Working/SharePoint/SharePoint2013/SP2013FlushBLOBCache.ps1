3d41c3583708cceb891329c871315843b0da3fea
chrisdee <chrisdee@github.com>
2014-06-03 14:45:29 +0200

    Added SharePoint  PowerShell Script To Flush BLOB Cache For Web Applications

---
+++ PowerShell/Working/SharePoint/SharePoint2013/SP2013FlushBLOBCache.ps1
@@ -0,0 +1,13 @@
+﻿## SharePoint Server: PowerShell Script To Flush BLOB Cache For Web Applications ##
+
+# Resource: http://technet.microsoft.com/en-us/library/gg277249(v=office.15).aspx
+
+# Environments: SharePoint Server 2013 Farms
+
+Add-PSSnapin "Microsoft.SharePoint.PowerShell" -ErrorAction SilentlyContinue
+
+$webApp = Get-SPWebApplication "https://webapp.yourdomain.com" #Change the web application name to match your environment
+
+[Microsoft.SharePoint.Publishing.PublishingCache]::FlushBlobCache($webApp)
+
+Write-Host "Flushed the BLOB cache for:" $webApp

90cd1c2e7698c45bc8d0dd8cb7691d7880bf21c2
chrisdee <chrisdee@github.com>
2014-06-03 14:15:01 +0200

    Added SharePoint PowerShell Test Send Email Scripts

---
+++ PowerShell/Working/SharePoint/SharePoint2013/SP2013TestEmailFunctionality.ps1
@@ -0,0 +1,24 @@
+﻿## SharePoint Server: PowerShell Script To Test SharePoint Send Mail Functionality ##
+
+<#
+
+overview: PowerShell Script that uses the Object Model to test sending email using the same 'SPUtility.SendMail' configured in the Incoming/Outgoing Central Admin E-Mail Settings
+
+Environments: SharePoint Server 2010 / 2013 Farms
+
+Usage: Edit the variables below and run the script
+
+#>
+
+Add-PSSnapin "Microsoft.SharePoint.PowerShell" -ErrorAction SilentlyContinue
+
+##### Begin Variables #####
+$spsite = "https://devinside.npe.theglobalfund.org" #Provide your SharePoint site details here
+$email = "yourname@youremail.com" #Add your recipient email address here
+$subject = "SharePoint SendMail Test" #Change your subject here
+$body = "Email sent via SharePoint site $spsite" #Change your body text here
+##### End Variables #####
+
+$site = New-Object Microsoft.SharePoint.SPSite "$spsite"
+$web = $site.OpenWeb()
+[Microsoft.SharePoint.Utilities.SPUtility]::SendEmail($web,0,0,$email,$subject,$body)

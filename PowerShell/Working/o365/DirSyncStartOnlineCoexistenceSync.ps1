85176e01c24dc6ddc488a7ae10ebc40c71b0925f
chrisdee <chrisdee@github.com>
2014-08-18 13:00:38 +0200

    Added Dir Sync 'Start-OnlineCoexistenceSync' PowerShell Script

---
+++ PowerShell/Working/o365/DirSyncStartOnlineCoexistenceSync.ps1
@@ -0,0 +1,5 @@
+﻿## PowerShell: Commandlet to manually start Active Directory Synchronization (Dir Sync) with Office 365 ##
+
+Add-PSSnapin "Coexistence-Configuration" -ErrorAction SilentlyContinue
+
+Start-OnlineCoexistenceSync

e17321ddb30838b75e2e2c571a7214ce3287f077
chrisdee <chrisdee@github.com>
2014-09-22 14:52:33 +0200

    Added SharePoint PowerShell Script to Get all 'SPShellAdmin' Role Users in a Farm

---
+++ PowerShell/Working/SharePoint/SharePoint2013/SP2013GetSPShellAdmin.ps1
@@ -0,0 +1,31 @@
+﻿## SharePoint Server: PowerShell Script to list all accounts granted the SPShell Admin Role (SPShellAdmin) in the Farm ##
+
+<#
+
+Overview: PowerShell Script that checks what accounts have been granted the SPShellAdmin access role against Config and Content Databases in the Farm
+
+Environments: SharePoint Server 2010 / 2013 Farms
+
+Resource: http://geekswithblogs.net/bjackett/archive/2011/01/04/sharepoint-2010-powershell-script-to-find-all-spshelladmins-with-database.aspx
+
+#>
+
+Add-PSSnapin "Microsoft.SharePoint.PowerShell" -ErrorAction SilentlyContinue
+
+# declare an array to store results
+$results = @()
+ 
+# fetch databases (only configuration and content DBs are needed)
+$databasesToQuery = Get-SPDatabase |
+    Where-Object {$_.Type -eq 'Configuration Database' -or $_.Type -eq 'Content Database'} |
+    Sort-Object –Property Name
+ 
+# for each database get spshelladmins and add db name and username to result
+$databasesToQuery |
+    ForEach-Object {$dbName = $_.Name;
+                    Get-SPShellAdmin -database $_.id |
+                        Sort-Object $_.username |
+                        ForEach-Object {$results += @{$dbName=$($_.username)}}}
+ 
+# sort results by db name and pipe to table with auto sizing of col width
+$results.GetEnumerator() | ft -AutoSize

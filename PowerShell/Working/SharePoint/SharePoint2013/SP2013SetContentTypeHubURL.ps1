8a5e7bb106d4e72e631031501489186d40507c31
chrisdee <chrisdee@github.com>
2014-11-13 17:17:17 +0100

    Added SharePoint PowerShell Script to Set the Content Type Hub URL

---
+++ PowerShell/Working/SharePoint/SharePoint2013/SP2013SetContentTypeHubURL.ps1
@@ -0,0 +1,12 @@
+﻿## SharePoint Server: PowerShell Script To Update The Content Type Hub URL For The Managed Metadata Service ##
+
+## Environments: SharePoint Server 2010 / 2013 Farms
+
+## Resource: http://www.sharepointanalysthq.com/2010/11/how-to-change-the-content-type-hub-url
+
+Add-PSSnapin "Microsoft.SharePoint.PowerShell" -ErrorAction SilentlyContinue
+
+$ServiceApplicationName = "Managed Metadata Service" #Change this to match your environment
+$ContentTypeHub = "https://contenttypehub.npe.theglobalfund.org" #Change this to the path of your Content Type Hub
+
+Set-SPMetadataServiceApplication -Identity $ServiceApplicationName -HubURI $ContentTypeHub

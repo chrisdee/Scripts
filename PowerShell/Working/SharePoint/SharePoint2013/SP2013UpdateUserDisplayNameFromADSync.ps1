15317cd5d215eec59f87e504e82caac6a201bb0d
chrisdee <chrisdee@github.com>
2014-12-24 12:33:20 +0100

    Added SharePoint PowerShell Script to Update User Display Name Values

--- PowerShell/Working/SharePoint/SharePoint2013/SP2013UpdateUserDisplayNameFromADSync.ps1
+++ PowerShell/Working/SharePoint/SharePoint2013/SP2013UpdateUserDisplayNameFromADSync.ps1
@@ -14,18 +14,18 @@ Add-PSSnapin "Microsoft.SharePoint.PowerShell" -ErrorAction SilentlyContinue
 
 ##### Begin Variables #####
 $SPWeb = "https://yourwebapp.com" #Change this to match your environment
-$Donain = "*YourDomain*" #Change this domain syntax to match your DC properties
+$Domain = "*YourDomain*" #Change this domain syntax to match your DC properties
 $LogFilePrior = "C:\BoxBuild\LogFilePrior.txt"
 $LogFileAfter = "C:\BoxBuild\LogFileAfter.txt"
 ##### End Variables #####
 
 ## Display all the user accounts for the SPWeb Prior to the AD Sync
 
-Get-SPUser –Web $SPWeb | Where-Object {$_.UserLogin –like $Donain} | Out-File -FilePath $LogFilePrior
+Get-SPUser –Web $SPWeb | Where-Object {$_.UserLogin –like $Domain} | Out-File -FilePath $LogFilePrior
 
 ## Display a count of all the user accounts for the SPWeb
 
-Get-SPUser –Web $SPWeb | Where-Object {$_.UserLogin –like $Donain} | Measure-Object | Out-File -FilePath $LogFilePrior -Append
+Get-SPUser –Web $SPWeb | Where-Object {$_.UserLogin –like $Domain} | Measure-Object | Out-File -FilePath $LogFilePrior -Append
 
 ## Now run the command to update the accounts with a Sync from Active Dirtecory (AD)
 
@@ -33,8 +33,8 @@ Get-SPUser –Web $SPWeb | Set-SPUser –SyncFromAD
 
 ## Display all the user accounts for the SPWeb After the AD Sync
 
-Get-SPUser –Web $SPWeb | Where-Object {$_.UserLogin –like $Donain} | Out-File -FilePath $LogFileAfter
+Get-SPUser –Web $SPWeb | Where-Object {$_.UserLogin –like $Domain} | Out-File -FilePath $LogFileAfter
 
 ## Display a count of all the user accounts for the SPWeb
 
-Get-SPUser –Web $SPWeb | Where-Object {$_.UserLogin –like $Donain} | Measure-Object | Out-File -FilePath $LogFileAfter -Append
+Get-SPUser –Web $SPWeb | Where-Object {$_.UserLogin –like $Domain} | Measure-Object | Out-File -FilePath $LogFileAfter -Append

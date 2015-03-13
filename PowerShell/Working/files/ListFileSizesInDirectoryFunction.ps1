7d49c5d1eeda255d81d696badd09c053d5ad38f0
chrisdee <chrisdee@github.com>
2015-03-13 10:35:41 +0100

    Added PowerShell List File Sizes In Directory Function

---
+++ PowerShell/Working/files/ListFileSizesInDirectoryFunction.ps1
@@ -0,0 +1,33 @@
+﻿## PowerShell: Function to Convert File Length Properties into a more Readable Human Format ##
+
+$properties = @(
+    'Name'
+    'Directory'
+    @{
+        Label = 'Size'
+        Expression = {
+            if ($_.Length -ge 1GB)
+            {
+                '{0:F2} GB' -f ($_.Length / 1GB)
+            }
+            elseif ($_.Length -ge 1MB)
+            {
+                '{0:F2} MB' -f ($_.Length / 1MB)
+            }
+            elseif ($_.Length -ge 1KB)
+            {
+                '{0:F2} KB' -f ($_.Length / 1KB)
+            }
+            else
+            {
+                '{0} bytes' -f $_.Length
+            }
+        }
+    }
+)
+
+##Usage Example:
+Get-ChildItem -Path '\\SPTALOS13\i$\MSSQL11.SQL13\MSSQL\DATA\' -Recurse -Include *.MDF -ErrorAction SilentlyContinue |
+Sort-Object -Property Length -Descending |
+Format-Table -Property $properties #|
+#Out-File 'C:\ztemp\files.txt'

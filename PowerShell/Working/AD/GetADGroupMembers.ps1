4a7e5896c18483b8c03a7bacc2921605d90ded25
chrisdee <chrisdee@github.com>
2015-04-01 14:45:54 +0200

    Added PowerShell Script to Get AD Group Members Details

---
+++ PowerShell/Working/AD/GetADGroupMembers.ps1
@@ -0,0 +1,43 @@
+﻿## PowerShell: Get All Groups And Members Associated With The Groups In Active Directory (AD) ##
+
+## Overview: PowerShell Script that uses the 'ActiveDirectory' PowerShell Module to Get AD Groups and all the Members User Details associated with the Groups
+
+## Usage: Edit the variables below and run the script
+
+### Start Variables ###
+$distinguishedName = "OU=Groups,OU=YourOU,DC=YourDomain,DC=com"
+$displayName = "*A*"
+$filePath = "C:\ztemp\ADSecurityGroups.csv"
+### End Variables ###
+
+Import-Module ActiveDirectory
+
+$Groups = (Get-AdGroup -filter *  -SearchBase $distinguishedName | Where {$_.name -like $displayName} | select name -expandproperty name)
+
+
+$Table = @()
+
+$Record = @{
+"Group Name" = ""
+"Name" = ""
+"Username" = ""
+}
+
+Foreach ($Group in $Groups)
+{
+
+$Arrayofmembers = Get-ADGroupMember -identity $Group -recursive | select name,samaccountname
+
+foreach ($Member in $Arrayofmembers)
+{
+$Record."Group Name" = $Group
+$Record."Name" = $Member.name
+$Record."UserName" = $Member.samaccountname
+$objRecord = New-Object PSObject -property $Record
+$Table += $objrecord
+
+}
+
+}
+
+$Table | export-csv $filePath -NoTypeInformation -Encoding "Default"

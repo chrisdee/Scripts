94f6e3cf5bdb98c0ccedb4d97d1066d9c3c9c494
chrisdee <chrisdee@github.com>
2015-06-10 16:04:25 +0200

    Added PowerShell ForEach Snippet for CSV Input Files

---
+++ PowerShell/Working/Snippets/ForEachFromCSVImport.ps1
@@ -0,0 +1,15 @@
+﻿## PowerShell: Script showing how to use a 'ForEach' loop for importing data from a CSV File ##
+
+$InputFilePath = "C:\BoxBuild\Scripts\SPSitesReport.csv" #Change this to match your environment
+
+$CsvFile = Import-Csv $InputFilePath
+
+ForEach ($line in $CsvFile)
+
+{ 
+
+#Add your command syntax here like the example below
+
+Write-Host $line.URL #In this example we display / write the value for a column called 'URL' in the CSV input file 
+
+}

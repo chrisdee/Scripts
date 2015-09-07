f7c0f27e3a808a704c768751ec2975a8e01d71a5
chrisdee <chrisdee@github.com>
2015-09-07 17:32:20 +0200

    Added PowerShell Function to Connect to MSOnline / Exchange Online

---
+++ PowerShell/Working/o365/ConnectToMSOnlineAndExchangeOnlineFunction.ps1
@@ -0,0 +1,21 @@
+﻿## MSOnline: PowerShell Function to Connect to MS Online and Exchange Online PowerShell Modules (o365) ##
+
+Function Connect-O365 {
+ 
+Param (
+  [Parameter(Mandatory=$true)]
+  $User
+  )
+ 
+  $Cred = Get-Credential -Credential $User
+ 
+  $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.outlook.com/powershell/ -Credential $Cred -Authentication Basic -AllowRedirection
+ 
+  Import-Module (Import-PSSession $Session -AllowClobber) -Global
+ 
+  Connect-MSOLService -Credential $Cred
+ 
+}
+
+## Call the function
+Connect-O365

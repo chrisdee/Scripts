0cf1331b47d6cf8ca50367bdd8ed4f818842470a
chrisdee <chrisdee@github.com>
2015-09-09 16:37:17 +0200

    Added PowerShell Script to Retrieve Certificate Store Details

---
+++ PowerShell/Working/certificates/GetCertificateDetails.ps1
@@ -0,0 +1,6 @@
+﻿## PowerShell: Script to list Certificate Details stored in a Local Machine / Currecnt User Certificate Store  ##
+
+Set-Location Cert:\LocalMachine\My #Set the path to the 'Local Computer' Personal certificate store
+#Set-Location Cert:\CurrentUser\My #Set the path to the 'Current User' Persoanl Certfiicate store
+
+Get-ChildItem | Format-Table Subject, FriendlyName, Thumbprint

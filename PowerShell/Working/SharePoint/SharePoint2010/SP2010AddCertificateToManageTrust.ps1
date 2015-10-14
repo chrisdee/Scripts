eb34fb9ccc8555b8e96fff8c0c82cbae35f11f38
chrisdee <chrisdee@github.com>
2015-10-14 16:41:08 +0200

    Added PowerShell Script to Add Certificates to SharePoint Trust

---
+++ PowerShell/Working/SharePoint/SharePoint2010/SP2010AddCertificateToManageTrust.ps1
@@ -0,0 +1,20 @@
+﻿## SharePoint Server: PowerShell Script to Add a Certificate File (.cer) to the Farm Trust ##
+
+## Overview: Adds a certificate (.cer) to the Farms trust relationship manager (/_admin/ManageTrust.aspx)
+
+## Environments: SharePoint Server 2010 / 2013 Farms
+
+## Usage: Edit the Variables to match your environment and run the script
+
+### Start Variables ###
+
+$CertPath = "C:\BoxBuild\Certs\WorkflowFarm.cer"
+$TrustName = "Workflow Manager Farm"
+
+### End Variables ###
+
+Add-PSSnapin "Microsoft.SharePoint.PowerShell" -ErrorAction SilentlyContinue
+
+$trustCert = Get-PfxCertificate $CertPath
+
+New-SPTrustedRootAuthority -Name $TrustName -Certificate $trustCert

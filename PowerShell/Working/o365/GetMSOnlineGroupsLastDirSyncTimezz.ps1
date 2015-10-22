278fb0d9e5355febccc5335eb6b4f7a8188dcebc
chrisdee <chrisdee@github.com>
2015-10-22 16:21:54 +0200

    Updates to resolve 'Binary File Not Found' errors on MSOnline DirSyncTime Scripts

---
+++ PowerShell/Working/o365/GetMSOnlineGroupsLastDirSyncTimezz.ps1
@@ -0,0 +1,18 @@
+﻿## MSOnline: PowerShell Script to Get All MSOnline Groups Last Sync Time via DirSync (o365) ##
+
+## Connect to MSOnline Tenant
+Import-Module MSOnline
+Import-Module MSOnlineExtended
+$cred=Get-Credential
+Connect-MsolService -Credential $cred
+
+## Retrieve the Groups that have a last DirSyncTime
+$objDistributionGroups = Get-MSolgroup -All | where lastdirsynctime -ne $null
+Foreach
+($objDistributionGroup in $objDistributionGroups)
+
+{
+
+Write-Output "$($objDistributionGroup.DisplayName + ', ' + $objDistributionGroup.EmailAddress + ', ' + $objDistributionGroup.LastDirSyncTime)"
+
+}
---
+++ PowerShell/Working/o365/GetMSOnlineGroupsWithoutLastDirSyncTimezz.ps1
@@ -0,0 +1,18 @@
+﻿## MSOnline: PowerShell Script to Get All MSOnline Groups that have not been Synchronised via DirSync (o365) ##
+
+## Connect to MSOnline Tenant
+Import-Module MSOnline
+Import-Module MSOnlineExtended
+$cred=Get-Credential
+Connect-MsolService -Credential $cred
+
+## Retrieve the Groups that don't have a last DirSyncTime
+$objDistributionGroups = Get-MSolgroup -All | where lastdirsynctime -eq $null
+Foreach
+($objDistributionGroup in $objDistributionGroups)
+
+{
+
+Write-host "$($objDistributionGroup.DisplayName + ', ' + $objDistributionGroup.EmailAddress)"
+
+}

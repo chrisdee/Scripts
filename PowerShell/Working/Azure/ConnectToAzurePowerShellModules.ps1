ff47e4116a23682d2831dc3ae561a9b11d6ee911
chrisdee <chrisdee@github.com>
2015-11-25 15:37:25 +0100

    Added PowerShell Connect to Azure PowerShell Modules Script

---
+++ PowerShell/Working/Azure/ConnectToAzurePowerShellModules.ps1
@@ -0,0 +1,21 @@
+﻿## Azure: PowerShell Commands For Importing and Connecting with the Azure PowerShell Modules (Azure and AzureRM) ##
+
+## Connecting to Azure Service Management ##
+
+Import-Module Azure
+
+Add-AzureAccount
+
+#Get-AzureVM
+#Get-AzureWebsite
+
+## Connecting to Azure Resource Management ##
+
+## Dependencies: Windows Management Framework 5.0 | http://www.microsoft.com/en-us/download/details.aspx?id=48729
+
+Import-AzureRM
+
+Login-AzureRmAccount
+
+#Get-AzureRmSubscription
+#Get-AzureRmSubscription –SubscriptionName "Microsoft Azure Enterprise" | Select-AzureRmSubscription

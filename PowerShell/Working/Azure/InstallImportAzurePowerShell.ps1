c27fa8c3105a02df55e6df3d9d7a4c96b80028c5
chrisdee <chrisdee@github.com>
2015-11-25 15:45:20 +0100

    Added PowerShell Install and Import Azure PowerShell Modules Script

---
+++ PowerShell/Working/Azure/InstallImportAzurePowerShell.ps1
@@ -0,0 +1,34 @@
+﻿## Azure: PowerShell Commands For Installing and Importing The Azure PowerShell Modules ##
+
+<# 
+
+Version: Azure PowerShell 1.0 (Preview)
+
+Dependencies: Windows Management Framework 5.0 | http://www.microsoft.com/en-us/download/details.aspx?id=48729
+
+Resources:
+
+https://github.com/Azure/azure-powershell
+
+https://azure.microsoft.com/en-us/documentation/articles/powershell-install-configure/#Install
+
+https://azure.microsoft.com/en-us/blog/azps-1-0-pre
+
+http://www.microsoft.com/en-us/download/details.aspx?id=48729 (Windows Management Framework 5.0)
+
+#>
+
+## Installing and Importing the Azure PowerShell Modules ##
+
+# Install all of the AzureRM.* modules
+Install-Module AzureRM
+
+Install-AzureRM
+
+Install-Module Azure
+
+# Import all of the AzureRM.* modules within the known semantic version range
+Import-AzureRM
+
+# Import Azure Service Management
+Import-Module Azure

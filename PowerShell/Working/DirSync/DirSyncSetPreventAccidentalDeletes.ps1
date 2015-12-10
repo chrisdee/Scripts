bce13f0ba64f6e357c3912ab923e878953eba5f2
chrisdee <chrisdee@github.com>
2015-12-10 10:20:01 +0100

    Added PowerShell DirSync Script to Configure Accidental Deletes Mechanism

---
+++ PowerShell/Working/DirSync/DirSyncSetPreventAccidentalDeletes.ps1
@@ -0,0 +1,13 @@
+﻿## DirSync: PowerShell Commands to Configure Accidental Deletion Functionality (Prevent Accidental Deletes) ##
+
+#Import the DirSync Module
+Import-Module Dirsync
+
+#Disable the Accidental Deletion Functionality
+Set-PreventAccidentalDeletes –Disable
+
+#Trigger a Full DirSync
+Start-OnlineCoexistenceSync -FullSync
+
+#Enable
+Set-PreventAccidentalDeletes -Enable –ObjectDeletionThreshold 500 #Change the threshold value to match your requirements

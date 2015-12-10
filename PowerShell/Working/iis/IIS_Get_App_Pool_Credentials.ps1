8898040266200547725bb3496570dfd6fb23576d
chrisdee <chrisdee@github.com>
2015-12-10 12:16:45 +0100

    Added PowerShell Script to Get IIS App Pool Identities and Passwords

---
+++ PowerShell/Working/iis/IIS_Get_App_Pool_Credentials.ps1
@@ -0,0 +1,3 @@
+﻿## IIS Server: Get Application Pool User Names (Identity) and Passwords ##
+
+Get-CimInstance -Namespace root/MicrosoftIISv2 -ClassName IIsApplicationPoolSetting -Property Name, WAMUserName, WAMUserPass | Select Name, WAMUserName, WAMUserPass

957fe212f1e9c6f2b6560b254b8d70a703632b3c
chrisdee <chrisdee@github.com>
2016-01-06 10:32:03 +0100

    Added PowerShell Script to Delete a SharePoint Online Site Collection

---
+++ PowerShell/Working/o365/SharePointOnline/SPOnlineDeleteSiteCollection.ps1
@@ -0,0 +1,17 @@
+﻿## SharePoint Online: PowerShell SharePoint Online Module Script to Delete a Site Collection (SPOnline) ##
+
+## Overview: PowerShell Script that uses the SharePoint Online Module 'Remove-SPOSite' cmdlet to delete a site collection. This site collection is then moved to the Recycle Bin
+
+## Note: To delete a site collection permanently, first move the site collection to the Recycle Bin by using the 'Remove-SPOSite' cmdlet, and then delete it by using the 'Remove-SPODeletedSite' cmdlet.
+
+## Usage: Replace the 'contoso' placeholder value under 'Connect-SPOService' with your own tenant prefix and provide the Site Collection URL under the '$SiteURL' variable
+
+## Resource: https://technet.microsoft.com/en-us/library/fp161377.aspx
+
+$SiteURL = "https://contoso.sharepoint.com/sites/sitetoremove"
+
+Import-Module Microsoft.Online.Sharepoint.PowerShell
+$credential = Get-credential 
+Connect-SPOService -url https://contoso-admin.sharepoint.com -Credential $credential 
+
+Remove-SPOSite -Identity $SiteURL -NoWait

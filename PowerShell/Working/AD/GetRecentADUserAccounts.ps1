ae14ff85a814b7cdb9a1e42db0bc830ca4b5a7a5
chrisdee <chrisdee@github.com>
2016-04-29 11:07:59 +0200

    Added PowerShell Script To Get Recently Created AD User Accounts

---
+++ PowerShell/Working/AD/GetRecentADUserAccounts.ps1
@@ -0,0 +1,7 @@
+﻿## Active Directory: PowerShell Script to Get Recently Created Users With CSV Output ##
+
+## Usage: Provide a numeric value to the 'AddDays' property, and provide a path to the 'Export-CSV' command to match your requirements before running the script
+
+Import-Module activedirectory
+$When = ((Get-Date).AddDays(-1)).Date #Change the 'AddDays' property to match the number of Days back you want to query
+Get-ADUser -Filter {whenCreated -ge $When} -Properties * | Select UserPrincipalName, DisplayName, GivenName, Surname, Title, EmailAddress, Department, OfficePhone, MobilePhone, Office, Company, Enabled, EmployeeNumber, @{N='Manager';E={$_.Manager.Substring($_.Manager.IndexOf("=") + 1, $_.Manager.IndexOf(",") - $_.Manager.IndexOf("=") - 1)}}, WhenCreated | Export-CSV "C:\BoxBuild\Scripts\RecentlyCreatedUsers.csv" -NoTypeInformation -Encoding "Default"

9ed3ce7c478d7473d3ebe0948a0d15defbc234c2
chrisdee <chrisdee@github.com>
2016-05-26 16:47:25 +0200

    Added PowerShell SharePoint Reset Search Index and Set Log Path Scripts

---
+++ PowerShell/Working/SharePoint/SharePoint2010/SP2010ResetSearchIndex.ps1
@@ -0,0 +1,54 @@
+﻿## SharePoint Server: PowerShell Script to Stop All Search Crawls and Reset the Search Index for All Content Sources ##
+
+<#
+
+Overview: PowerShell script that Stops all currently running crawls (including continuous crawls), and then resets the Search Index for all content sources
+
+Note: Sometimes the Crawl Status might get stuck with a status message of 'Stopping'. This can often be resolved by attempting the script again
+
+Environments: SharePoint Server 201 / 2013 Farms
+
+Resources: 
+
+http://www.sharepointdiary.com/2015/05/reset-search-index-in-sharepoint-2013-using-powershell.html
+
+http://www.c-sharpcorner.com/blogs/force-stop-and-then-start-a-full-crawl-search-in-sharepoint-2013
+
+https://technet.microsoft.com/en-us/library/jj219802.aspx#Disable_for_all
+
+#>
+
+Add-PSSnapin Microsoft.SharePoint.PowerShell –ErrorAction SilentlyContinue
+
+#Get Search service application
+$ssa = Get-SPEnterpriseSearchServiceApplication
+
+#Get the crawl status of the Search service application
+Get-SPEnterpriseSearchCrawlContentSource -SearchApplication "Search Service Application" | select Name, CrawlStatus #Replace the default 'Search Service Application' name under '-SearchApplication' if different for your environment
+
+#Disable continuous crawls for all content sources if enabled
+$SSA =  Get-SPEnterpriseSearchServiceApplication
+$SPContentSources = $SSA | Get-SPEnterpriseSearchCrawlContentSource | WHERE {$_.Type -eq "SharePoint"} 
+foreach ($cs in $SPContentSources) 
+{ 
+  $cs.EnableContinuousCrawls = $false 
+  $cs.Update() 
+}
+
+#Stop all currently running crawls
+Get-SPEnterpriseSearchCrawlContentSource -SearchApplication "Search Service Application" | ForEach-Object {  
+    if ($_.CrawlStatus -ne "Idle")  
+    {  
+        Write-Host "crawl stopping currently for content source $($_.Name)..."  
+        $_.StopCrawl()  
+         
+        do { Start-Sleep -Seconds 1 }  
+        while ($_.CrawlStatus -ne "Idle")  
+    }
+    }
+
+#Recheck the crawl status of the Search service application (Should be 'Idle')
+Get-SPEnterpriseSearchCrawlContentSource -SearchApplication "Search Service Application" | select Name, CrawlStatus
+
+#Reset the search index completely for all content sources
+(Get-SPEnterpriseSearchServiceApplication).reset($true, $true)
---
+++ PowerShell/Working/SharePoint/SharePoint2010/SP2010SetLoggingLocationPaths.ps1
@@ -0,0 +1,11 @@
+﻿## SharePoint Server: PowerShell Script to Set the Path for Diagnostic Logs (ULS) and Usage and Health Data ##
+
+## Environments: SharePoint Server 201 / 2013 Farms
+
+Add-PSSnapin "Microsoft.SharePoint.PowerShell" -ErrorAction SilentlyContinue
+
+#Configure diagnostic logging path (ULS)
+Set-SPDiagnosticConfig -LogLocation "D:\Data\SharePoint\Logs\ULS"
+
+#Configure usage and health data collection
+Set-SPUsageService -UsageLogLocation "D:\Data\SharePoint\Logs\Usage"

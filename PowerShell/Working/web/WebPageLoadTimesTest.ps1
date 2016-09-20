1733eee89761740064b61fd88cd2c156b98f716c
chrisdee <christopherdee@hotmail.com>
2016-09-20 12:08:45 +0200

    Added PowerShell Script To Check / Monitor Page Load Times

---
+++ PowerShell/Working/web/WebPageLoadTimesTest.ps1
@@ -0,0 +1,27 @@
+﻿## PowerShell: PowerShell Function that Measures Page Load Times and HTTP Protocol Status Codes ##
+
+<#
+
+Overview: PowerShell Function that uses the 'System.Net.WebClient' to Measure Page Load Times, and HTTP Protocol Status Codes over a specified number of Times
+
+Usage Example:
+
+MeasurePageLoad "https://google.com" -Times 10
+
+#>
+
+Function MeasurePageLoad {
+
+param($URL, $Times)
+$i = 1
+While ($i -lt $Times)
+{$Request = New-Object System.Net.WebClient
+$Request.UseDefaultCredentials = $true
+$Start = Get-Date
+$PageRequest = $Request.DownloadString($URL)
+$TimeTaken = ((Get-Date) – $Start).Totalseconds
+$StatusCode = [int][system.net.httpstatuscode]::ok
+$Request.Dispose()
+Write-Host Request $i took $TimeTaken Seconds with a $StatusCode  HTTP Status Code -ForegroundColor Green
+$i ++}
+}

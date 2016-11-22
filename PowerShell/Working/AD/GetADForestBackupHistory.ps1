7d36eb5f6e43ec34cd64bf3fca8cfbe76f024aeb
chrisdee <christopherdee@hotmail.com>
2016-11-22 17:22:58 +0100

    Added PowerShell AD Scripts For Backup Checks And Object Searches

---
+++ PowerShell/Working/AD/GetADForestBackupHistory.ps1
@@ -0,0 +1,54 @@
+﻿## Active Directory: PowerShell Script to check Active Directory Backup Dates on All Domains in a Forest ##
+
+<#
+
+Overview: PowerShell Script to check Active Directory Backup Dates on All Domains in a Forest. Returns the Domain; Domain Controller/s; and Partitions that were backed up
+
+Usage: Edit the '$backup_age_threshold' variable to match your requirements and run the query
+
+Requires: Active Directory PowerShell Module
+
+Resource: https://www.shellandco.net/active-directory-backup-check
+
+#>
+
+Import-Module "ActiveDirectory"
+
+$myForest = [System.DirectoryServices.ActiveDirectory.Forest]::GetCurrentForest()
+$forestDN = (($myforest.Name).split(".")|%{"DC=$_"}) -join ","
+
+$domain_list = $myForest.Domains.Name
+$domainControllers = $myForest.GlobalCatalogs.Name
+
+$array = @()
+$backup_age_threshold = "1" #Change this threshold days value to match your requirements
+
+$domain_list | % {
+    $domain_fqdn = $_
+    $dcname = Get-ADDomainController -Discover -Service ADWS -DomainName $domain_fqdn | select Hostname -first 1 | % { $_.Hostname }
+    $Partitions = (Get-ADRootDSE -Server $domain_fqdn).namingContexts
+    $Partitions | % {
+        $Partition = $_
+        $object = Get-ADObject -Identity $Partition -Properties msDS-ReplAttributeMetaData -Server $dcname
+        $Object."msDS-ReplAttributeMetaData" | ForEach-Object {
+            $MetaData = [XML]$_.Replace("`0","")
+            $MetaData.DS_REPL_ATTR_META_DATA | ForEach-Object {
+                If ($_.pszAttributeName -eq "dSASignature") {
+                    $backup_date = Get-Date $_.ftimeLastOriginatingChange -format "dd.MM.yyyy HH:mm:ss"
+                    $backup_age = ((Get-date) - (Get-Date $_.ftimeLastOriginatingChange)).TotalDays
+                    $Properties = @{domain=$domain_fqdn;dc=$dcname;partition=$Partition;backup_date=$backup_date;backup_age=$backup_age}
+                    $Newobject = New-Object PSObject -Property $Properties
+                    $array += $Newobject
+                }
+            }
+        }
+    }
+}
+$array | % {
+	if ($_.backup_age -gt $backup_age_threshold) {
+		write-host $_.domain "/" ($_.dc) "/" "Partition" $_.partition "/" "Backup is older than the configured threshold of" $backup_age_threshold "days" "/" "Last backup occured on" $_.backup_date -foregroundcolor red
+	}
+	else {
+		write-host $_.domain "/" ($_.dc) "/" "Partition" $_.partition "/" "Backup is OK" "/" "Last backup occured on" $_.backup_date -foregroundcolor green
+	}
+}
---
+++ PowerShell/Working/AD/GetADObjectsInAForest.ps1
@@ -0,0 +1,37 @@
+﻿## Active Directory: PowerShell Script to Search for Forest Wide Objects ##
+
+<#
+
+Overview: PowerShell Script to search for objects forest wide. The search is based on a LDAP filter
+
+Usage: Edit the parameters under the object search filter under '$objSearcher.Filter' and run the query
+
+Requires: Active Directory PowerShell Module
+
+Resource: https://www.shellandco.net/search-an-object-forest-wide
+
+#>
+
+Import-Module "ActiveDirectory"
+
+#Get Domain List
+$objForest = [System.DirectoryServices.ActiveDirectory.Forest]::GetCurrentForest()
+$DomainList = @($objForest.Domains | Select-Object Name)
+$Domains = $DomainList | foreach {$_.Name}
+
+#Act on each domain
+foreach($Domain in ($Domains)) {
+	Write-Host "Checking $Domain" -fore red
+	$ADsPath = [ADSI]"LDAP://$Domain"
+	$objSearcher = New-Object System.DirectoryServices.DirectorySearcher($ADsPath)
+	#The filter
+	$objSearcher.Filter = "(&(objectCategory=user)(mail=*toto*))" ##Change this 'Filter' to match what your search scope on AD Object Category and Attribute
+	$objSearcher.SearchScope = "Subtree"
+	
+	$colResults = $objSearcher.FindAll()
+	
+	foreach ($objResult in $colResults) {
+		$objArray = $objResult.GetDirectoryEntry()
+		write-host $objArray.DistinguishedName ";" $objArray.mail ";" $objArray.ProxyAddresses "`r"
+	}
+}

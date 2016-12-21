31e25205bc6ee8b05df3996d8c982cf5912c4f04
chrisdee <christopherdee@hotmail.com>
2016-12-21 15:24:14 +0100

    Added PowerShell Script To Get Azure RM Application Details

---
+++ PowerShell/Working/Azure/GetAzureRmADApplication.ps1
@@ -0,0 +1,9 @@
+﻿## AzureRM: PowerShell Script to Get Azure AD Application Details with Azure Resource Manager ##
+
+Import-Module "AzureRM"
+
+Login-AzureRmAccount
+
+#Get-AzureRmADApplication
+#Get-AzureRmADApplication -DisplayNameStartWith "zz" #Change this to match your application Display Name
+Get-AzureRmADApplication | Select DisplayName, ReplyUrls | Format-Table

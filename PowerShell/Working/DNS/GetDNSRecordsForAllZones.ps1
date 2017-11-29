0bf1ac8602fba4559eed7d13d8ca631e53eaf9e3
chrisdee <christopherdee@hotmail.com>
2017-11-29 12:07:50 +0100

    Added PowerShell Scripts For DNS Server Records

---
+++ PowerShell/Working/DNS/GetDNSRecordsForAllZones.ps1
@@ -0,0 +1,22 @@
+﻿## DNS: PowerShell Script to List All DNS Records in Each Zone on a Windows DNS Server ##
+
+<#
+
+Overview: PowerShell Script to list all DNS records in each zone on a Windows DNS server (includes sub-zones)
+
+Note:  Script works on DNS role servers, along with AD / DNS role servers
+
+Usage: Provide your DNS server under the '$DNSServer' variable and edit the '$Results' output properties to match your environment
+
+Resource: http://sigkillit.com/2015/10/27/list-all-dns-records-with-powershell
+
+#>
+
+$DNSServer = "YourServerName" #Provide your DNS Server Name or IP address here
+$Zones = @(Get-DnsServerZone -ComputerName $DNSServer)
+ForEach ($Zone in $Zones) {
+	Write-Host "`n$($Zone.ZoneName)" -ForegroundColor "Green"
+	$Results = $Zone | Get-DnsServerResourceRecord -ComputerName $DNSServer
+    echo $Results >  "C:\BoxBuild\DNS\$($Zone.ZoneName).txt"
+	#echo $Results | Export-Csv "C:\BoxBuild\DNS\$($Zone.ZoneName).csv" -NoTypeInformation
+}
---
+++ PowerShell/Working/DNS/UpdateDNSARecordIPAddress.ps1
@@ -0,0 +1,22 @@
+﻿## DNS: PowerShell Script to Update an IP Address for an 'A' Record on a Windows DNS Server ##
+
+<#
+
+Overview: PowerShell Script to Update an IP Address for an 'A' Record on a Windows DNS Server
+
+Note:  Script works on DNS role servers, along with AD / DNS role servers
+
+Usage: Provide your variables to match your requirements and run the script
+
+#>
+
+### Start Variables ###
+$DNSName = "YourDNSName"
+$DNSZoneName = "YourDomain.com"
+$IPAddress = "10.0.0.1"
+### End Variables ###
+
+$oldobj = get-dnsserverresourcerecord -name $DNSName -zonename $DNSZoneName -rrtype "A"
+$newobj = get-dnsserverresourcerecord -name $DNSName -zonename $DNSZoneName -rrtype "A"
+$newobj.recorddata.ipv4address=[System.Net.IPAddress]::parse($IPAddress)
+Set-dnsserverresourcerecord -newinputobject $newobj -oldinputobject $oldobj -zonename $DNSZoneName -passthru
